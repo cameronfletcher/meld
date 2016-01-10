0e03c25a0e00e747220ac00548e3e3cf29d8eb4f
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2016-01-10 15:05:27 +0100

    #2 Changed version repository to initialize per connection string

---
+++ src/tests/Meld.Tests/Feature/SqlServerDatabaseUpgrade.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="SqlServerDatabaseUpgrade.cs" company="Meld contributors">
+//  Copyright (c) Meld contributors. All rights reserved.
+// </copyright>
+
+namespace Meld.Tests.Feature
+{
+    public class SqlServerDatabaseUpgrade
+    {
+        // can upgrade database
+        // rollback on error is atomic
+    }
+}
